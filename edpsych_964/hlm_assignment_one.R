c7c33e25eee54fd32a237e4cceb9258f5f1b95fc
Drew McDermott <dmcdermott@edanalytics.org>
2016-02-11 09:10:52 -0600

    added code for question 3 and 4, formatted output

--- edpsych_964/hlm_assignment_one.R
+++ edpsych_964/hlm_assignment_one.R
@@ -19,6 +19,8 @@ ea_start()
 library(foreign)
 library(stargazer)
 library(data.table)
+library(dplyr)
+library(broom)
 
 #############
 # set parms #
@@ -61,37 +63,69 @@ summary(m2)
 analysis_set[, schnum_f := as.factor(schnum)]
 
 # calculate an ANOVA model for SES across schools
-m3 <- aov(math ~ schnum_f, data = analysis_set)
+m3 <- aov(lm(ses ~ schnum_f, data = analysis_set))
 
-# summarize m3
-summary(m3)
+test = aov(math ~ Error(schnum_f), data = analysis_set)
+summary(test)
 
-m4 <- aov(math ~ Error(schnum_f), data = analysis_set)
+m3_tidy <- tidy(m3)
 
-ztest <- lm(math ~ schnum_f, data = analysis_set)
-ztest2 <- anova(ztest)
+##############################################
+# question 3: between and within-regressions #
+##############################################
 
+# take mean of ses and math score by school
+school_means <- analysis_set[, list(ses_mean = mean(ses),
+                                    math_mean = mean(math)), by = schnum]
 
+# run between groups regression - SES as predictor of math score at school level
+m4 <- lm(math_mean ~ ses_mean, data = school_means)
+
+# summarize m4
+summary(m4)
+
+# run within groups regression - average school math score and group-centered SES as predictor of math score at individual level
+m5 <- lm(math ~ math_1 + sesgc, data = analysis_set)
+
+# summarize m5
+summary(m5)
+
+####################################################
+# question 4: seperate regressions for each school #
+####################################################
+
+# break up analysis_set by school - run regression with SES as predictor of math score for each school
+school_models <- analysis_set %>% group_by(schnum) %>% do(tidy(lm(math ~  ses, data = .)))
 
 #################################
 # output models using stargazer #
 #################################
 
-# output models for question 1
-stargazer(m1, m2, type="html",
-          dep.var.labels = c("Miles/(US) gallon","Fast car (=1)"),
-          covariate.labels=c("Gross horsepower","Rear axle ratio","Four foward gears",
-                             "Five forward gears","Type of transmission (manual=1)"), out="models.htm")
-
-
-##########
-# export #
-##########
-
-# export
 if (opt_exp == 1) { 
   
-  ea_write( , ".csv")
+  # output models for question 1
+  stargazer(m1, m2, type = "html",
+            dep.var.labels = "Student Math Score",
+            covariate.labels = c("Student SES", "SES (Group-Centered)", "SES (Grand-Centered)"),
+            report = "vc*s",
+            out = "C:/Users/Drew/Dropbox/course_data/question1_models.htm")
+  
+  # output table of ANOVA results for question 2
+  ea_write(m3_tidy, "C:/Users/Drew/Dropbox/course_data/question2_anova.csv")
+  
+  
+  # output models for question 3
+  stargazer(m1, m4, m5, type = "html",
+            dep.var.labels = c("Student Math Score", "School Mean Math Score", "Student Math Score"),
+            covariate.labels = c("Student SES", "School Mean SES", "School Mean Math Score", "SES (Group-Centered)"),
+            report = "vc*s",
+            out = "C:/Users/Drew/Dropbox/course_data/question3_models.htm")
+  
+  # output table of school-level models for question 4
+  ea_write(school_models, "C:/Users/Drew/Dropbox/course_data/question4_models.csv")
   
 }
+
+
+
 
