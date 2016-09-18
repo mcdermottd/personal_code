9a25474766c458da112715878040fa3116055be2
Drew McDermott <dmcdermott@edanalytics.org>
2016-09-18 12:24:16 -0700

    updated code template

--- code_template.R
+++ code_template.R
@@ -1,46 +1,73 @@
-######################################################################
+######################################################################.
 # notes:
 # - purpose:
 # - inputs:
 # - outputs:
 # - keywords:
 # - general:
-######################################################################
+######################################################################.
 
-#######################################
-# load packages and clear objects/log #
-#######################################
+#==============================================#
+# ==== load packages and clear objects/log ====
+#==============================================#
 
   # load easimple and clear objects log
   library(easimple)
   ea_start()
   
   # load packages
-  library(weights)
-  library(dplyr)
-  library(reshape2)
+  library(readxl)
   library(data.table)
   library(eaanalysis)
 
-#############
-# set parms #
-#############
+#====================#
+# ==== set parms ====
+#====================#
 
+  # store timestamp
+  p_timestamp  <- ea_timestamp()
+  
+  # set project folder (under should be data, documents, results, qc)
+  p_dir_root <- paste0("C:/Users/Drew/Dropbox/analysis_data/")
+  
+  # set output directories
+  p_out_dir_recent <- paste0(p_dir_root, "data/most_recent/")
+  p_out_dir_date   <- paste0(p_dir_root, "data/by_date/", p_timestamp, "/")
+  
   # output toggle
-  opt_exp <- 0
+  p_opt_exp <- 0
 
-#############
-# load data #
-#############
+#====================#
+# ==== load data ====
+#====================#
+  
+  # load xwalks - model, test range, demo
+  in_model_xwalk <- data.table(read_excel(paste0(p_dir_root, "documents/xwalks/model_xwalk.xlsx")))
+  
+  # load data
+  in_raw_data <- ea_load(paste0(p_dir_root, "data/raw_data_file.rdata"))
 
-##########
-# export #
-##########
+  
+  
+  
+#=================#
+# ==== export ====
+#=================#
 
   # export
-  if (opt_exp == 1) { 
+  if (p_opt_exp == 1) { 
     
-    ea_write( , ".csv")
+    # create directories
+    dir.create(path = p_out_dir_recent, showWarnings = FALSE, recursive = TRUE)
+    dir.create(path = p_out_dir_date,   showWarnings = FALSE, recursive = TRUE)
+
+    # export data as rdata file
+    save(out_data, file = paste0(p_out_dir_recent, "out_data_file.rdata"), compress = TRUE)
+    save(out_data, file = paste0(p_out_dir_date,   "out_data_file.rdata"), compress = TRUE) 
+    
+    # export data as csv file
+    ea_write(out_data, paste0(p_out_dir_recent, "out_data_file.csv"))
+    ea_write(out_data, paste0(p_out_dir_date,   "out_data_file.csv"))   
     
   }
 
