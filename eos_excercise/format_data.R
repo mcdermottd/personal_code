abf9346104f808114d897ca9220865d27f968ed4
Drew McDermott <dmcdermott@edanalytics.org>
2016-09-18 20:41:21 -0700

    fix issue with total students var (0 instead of NA)

--- eos_excercise/format_data.R
+++ eos_excercise/format_data.R
@@ -3,7 +3,7 @@
 # - purpose:
 # - inputs:
 # - outputs:
-# - keywords:
+# - keywords: #brule
 # - general:
 ######################################################################.
 
@@ -59,6 +59,11 @@
   setnames(eos_summ_data, gsub("/", "_",       colnames(eos_summ_data)))
   setnames(eos_summ_data, gsub("_\\(1,0)", "", colnames(eos_summ_data)))
   
+  # change total students var to NA if other vars are missing #brule
+  eos_summ_data[is.na(closed_gaps_13_14), total_students_added_to_ap_ib_over_baseline_13_14 := NA]
+  eos_summ_data[is.na(closed_gaps_14_15), total_students_added_to_ap_ib_over_baseline_14_15 := NA]
+  eos_summ_data[is.na(closed_gaps_15_16), total_students_added_to_ap_ib_over_baseline_15_16 := NA]
+
 #=======================================#
 # ==== fix schools with data issues ====
 #=======================================#
@@ -67,24 +72,24 @@
   # ==== * negative value in num bm 13_14 variable (set includes 3 schools: 30, 31, and 80) ====
   #=============================================================================================#
   
-    # add negative bm students to totals
+    # add negative bm students to totals #brule
     eos_summ_data[num_benchmark_students_added_over_baseline_13_14 < 0, total_students_added_to_ap_ib_over_baseline_13_14 := 
                     total_students_added_to_ap_ib_over_baseline_13_14 - num_benchmark_students_added_over_baseline_13_14]
     
-    # re-calculate percentages
+    # re-calculate percentages #brule
     eos_summ_data[num_benchmark_students_added_over_baseline_13_14 < 0, perc_underrep_students_added_to_ap_ib_over_baseline_13_14 := 
                     num_underrep_students_added_to_ap_ib_over_baseline_13_14 / total_students_added_to_ap_ib_over_baseline_13_14]
     eos_summ_data[num_benchmark_students_added_over_baseline_13_14 < 0, perc_benchmark_students_added_over_baseline_13_14 := 
                     0 / total_students_added_to_ap_ib_over_baseline_13_14]
       
-    # set negative value to 0 
+    # set negative value to 0 #brule
     eos_summ_data[num_benchmark_students_added_over_baseline_13_14 < 0, num_benchmark_students_added_over_baseline_13_14 := 0]
     
   #========================================================================#
   # ==== * ur value in 14_15 but others NA (set includes 1 school: 27) ====
   #========================================================================#
     
-    # change inconsistent ur value to NA
+    # change inconsistent ur value to NA #brule
     eos_summ_data[school_id == 27, num_underrep_students_added_to_ap_ib_over_baseline_14_15 := NA]
     
 #===============================#
